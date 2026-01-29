e63199e85478dfbabce59068455434b12bfc8fbc
emi-test-1 <198296268+emi-test-1@users.noreply.github.com>
2026-01-29 10:37:20 -0300

    upd

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+
+const envText = import.meta.env.VITE_SOME_TEXT;
+
 </script>
 
 <template>
@@ -8,6 +11,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
+      {{ envText }}
       <HelloWorld msg="You did it!" />
     </div>
   </header>
